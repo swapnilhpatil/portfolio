0d2bb7e26e2db6680e10de667bc12414bed176b5
swapnil <swapnilhpatil9999@gmail.com>
2023-09-10 23:51:30 +0530

    Added initial home, project, project details and contact pages routes changes

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to My Portfolio</h1>
+      {/* Add your introductory content here */}
+    </div>
+  );
+};
+
+export default HomePage;
