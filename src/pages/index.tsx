b18673aaa9ca16f4bb53f59f851382ecb5039a44
swapnil <swapnilhpatil9999@gmail.com>
2023-09-11 01:04:21 +0530

    Added header section component and global css file changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Example from '@/components/header';
 
 const HomePage: React.FC = () => {
   return (
     <div>
       <h1>Welcome to My Portfolio</h1>
       {/* Add your introductory content here */}
+      <Example />
     </div>
   );
 };
